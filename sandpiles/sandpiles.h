5523ed86b93108440715ab66d8f06ca45e360383
Alberto Riffaud <alriffaud@gmail.com>
2024-09-27 09:09:19 -0300

    Modification of sandpiles.h and 0-sandpiles.c files.

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// Function prototypes
+/* Function prototypes */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 bool is_stable(int grid[3][3]);

902b6289f3626d28969e51059c825d2b3ce26616
Alberto Riffaud <alriffaud@gmail.com>
2024-12-20 11:27:24 -0300

    Modification of 0-rain.py file.

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -14,29 +14,19 @@ def rain(walls):
         return 0
 
     n = len(walls)
+    left = [0] * n
+    right = [0] * n
+    water = 0
 
-    i = 0
-    while walls[i] == 0 and i < n:
-        i += 1
+    left[0] = walls[0]
+    for i in range(1, n):
+        left[i] = max(left[i - 1], walls[i])
 
-    if i == n:
-        return 0
+    right[n - 1] = walls[n - 1]
+    for i in range(n - 2, -1, -1):
+        right[i] = max(right[i + 1], walls[i])
 
-    left = walls[i]
-    right = 0
-    empty = 0
-    total = 0
+    for i in range(n):
+        water += min(left[i], right[i]) - walls[i]
 
-    for j in range(i + 1, n):
-        if walls[j] == 0:
-            empty += 1
-        else:
-            right = walls[j]
-            minimo = min(left, right)
-            total += minimo * empty
-            # Restart values
-            left = right
-            empty = 0
-            right = 0
-
-    return total
+    return water
